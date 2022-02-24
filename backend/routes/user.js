2d0fb6f227a387e1bdb739e86a3fb8a4ecb97639
saboodev <sabrina_desantis@hotmail.fr>
2022-02-24 11:43:45 +0100

    ajout crypto-js et password-validator

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// middleware password pour un mdp fort
+const password = require("../middleware/password");
+
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', password, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
